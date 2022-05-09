df7bd8c1c7da47acc4fd57141162d6967f49b067
unknown <iamhasan9501@gmail.com>
2022-05-10 00:06:20 +0600

    test commit, after error solved

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
 import './Products.css';
-
+//products.json
 const Products = () => {
     const [products, setProducts] = useState([]);
     useEffect(() => {
-        fetch('products.json')
+        fetch('')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
