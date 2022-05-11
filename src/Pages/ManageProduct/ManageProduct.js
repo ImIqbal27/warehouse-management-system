0c906833fc0ff587fb286129811987dc66a8f07b
unknown <iamhasan9501@gmail.com>
2022-05-12 03:06:22 +0600

    single data/product load using id

--- src/Pages/ManageProduct/ManageProduct.js
+++ src/Pages/ManageProduct/ManageProduct.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './ManageProduct.css';
 
 const ManageProduct = () => {
     const { productId } = useParams();
+    const [product, setProduct] = useState({});
+
+    useEffect(() => {
+        const url = `http://localhost:5000/products/${productId}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setProduct(data))
+
+    }, [])
     return (
         <div>
-            <h2>Manage Product= {productId}</h2>
+            <h2>Manage Product= {product.name}</h2>
         </div>
     );
 };
